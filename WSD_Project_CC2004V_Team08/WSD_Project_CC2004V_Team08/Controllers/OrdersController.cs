881496d09176f59340a616e697a2097187336906
NEO XIN MEI <neo_xin_mei@connect.ite.edu.sg>
2021-08-22 19:30:58 +0800

    Database Setup

---
+++ WSD_Project_CC2004V_Team08/WSD_Project_CC2004V_Team08/Controllers/OrdersController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WSD_Project_CC2004V_Team08.Models
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrdersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Orders
+        [HttpGet]
+        public IEnumerable<Order> GetOrders()
+        {
+            return _context.Orders;
+        }
+
+        // GET: api/Orders/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOrders([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var orders = await _context.Orders.FindAsync(id);
+
+            if (orders == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(orders);
+        }
+
+        // PUT: api/Orders/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOrders([FromRoute] int id, [FromBody] Order orders)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != orders.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(orders).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrdersExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Orders
+        [HttpPost]
+        public async Task<IActionResult> PostOrders([FromBody] Order orders)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Orders.Add(orders);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetOrders", new { id = orders.ID }, orders);
+        }
+
+        // DELETE: api/Orders/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrders([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var orders = await _context.Orders.FindAsync(id);
+            if (orders == null)
+            {
+                return NotFound();
+            }
+
+            _context.Orders.Remove(orders);
+            await _context.SaveChangesAsync();
+
+            return Ok(orders);
+        }
+
+        private bool OrdersExists(int id)
+        {
+            return _context.Orders.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ WSD_Project_CC2004V_Team08/WSD_Project_CC2004V_Team08/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WSD_Project_CC2004V_Team08.Models
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        [Required]
+        public string password { get; set; }
+    }
+}
